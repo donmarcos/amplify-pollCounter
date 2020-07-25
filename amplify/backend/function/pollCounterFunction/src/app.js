4366c89a12929a3ad0067798f355998b2846e001
unknown <marcos.alois.oliva@gmail.com>
2020-07-25 12:04:11 -0700

    AWS Amplify debugging node9

--- amplify/backend/function/pollCounterFunction/src/app.js
+++ amplify/backend/function/pollCounterFunction/src/app.js
@@ -151,37 +151,7 @@ app.get(path + '/object' + hashKeyPath + sortKeyPath, function (req, res) {
 });
 
 
-/************************************
- * HTTP put method for insert object *
- *************************************/
 
-app.put(path, function (req, res) {
-
-  if (userIdPresent) {
-    req.body['userId'] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
-  }
-
-  let putItemParams = {
-    TableName: tableName,
-    Item: req.body
-  }
-  dynamodb.put(putItemParams, (err, data) => {
-    if (err) {
-      res.statusCode = 500;
-      res.json({
-        error: err,
-        url: req.url,
-        body: req.body
-      });
-    } else {
-      res.json({
-        success: 'put call succeed!',
-        url: req.url,
-        data: data
-      })
-    }
-  });
-});
 
 /************************************
  * HTTP post method for insert object *
@@ -214,7 +184,7 @@ app.post(path, function (req, res) {
   console.warm("***** WARM log  *****")
 
   res.json({
-    success: 'post call succeed!',
+    success: 'post call succeed! Marcos ',
     url: req.url,
     data: data
   })
@@ -223,7 +193,7 @@ app.post(path, function (req, res) {
 
 });
 
-
+/*
 app.postx(path, function (req, res) {
 
   console.log("***** POST  pollCounter *****")
@@ -279,7 +249,7 @@ app.postx(path, function (req, res) {
     }
   })
 
-  /*
+  
   dynamodb.put(putItemParams, (err, data) => {
     if(err) {
       res.statusCode = 500;
@@ -288,59 +258,13 @@ app.postx(path, function (req, res) {
       res.json({success: 'post call succeed!', url: req.url, data: data})
     }
   });
-  */
+  
 
 });
+*/
 
-/**************************************
- * HTTP remove method to delete object *
- ***************************************/
 
-app.delete(path + '/object' + hashKeyPath + sortKeyPath, function (req, res) {
-  var params = {};
-  if (userIdPresent && req.apiGateway) {
-    params[partitionKeyName] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
-  } else {
-    params[partitionKeyName] = req.params[partitionKeyName];
-    try {
-      params[partitionKeyName] = convertUrlType(req.params[partitionKeyName], partitionKeyType);
-    } catch (err) {
-      res.statusCode = 500;
-      res.json({
-        error: 'Wrong column type ' + err
-      });
-    }
-  }
-  if (hasSortKey) {
-    try {
-      params[sortKeyName] = convertUrlType(req.params[sortKeyName], sortKeyType);
-    } catch (err) {
-      res.statusCode = 500;
-      res.json({
-        error: 'Wrong column type ' + err
-      });
-    }
-  }
 
-  let removeItemParams = {
-    TableName: tableName,
-    Key: params
-  }
-  dynamodb.delete(removeItemParams, (err, data) => {
-    if (err) {
-      res.statusCode = 500;
-      res.json({
-        error: err,
-        url: req.url
-      });
-    } else {
-      res.json({
-        url: req.url,
-        data: data
-      });
-    }
-  });
-});
 app.listen(3000, function () {
   console.log("App started")
 });
